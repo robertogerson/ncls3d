c005c72c7e9b099454c687de2001f67151e15991
Roberto Azevedo <robertogerson@telemidia.puc-rio.br>
2015-02-06 15:42:03 -0200

    doc: Add copyright/license message.
    * README: Likewise.
    * TODO: Likewise.
    * get_deps.lua: Likewise.
    * ncls3d.lua: Likewise.
    * templates_xml.lua: Likewise.
    * tests/link_set_bounds.ncl: Likewise.
    * tests/links_set.ncl: Likewise.
    * tests/links_set_out.ncl: Likewise.
    * tests/media.ncl: Likewise.
    * tests/media_dynamic_depth.ncl: Likewise.
    * tests/media_dynamic_depth_heuristics.ncl: Likewise.
    * tests/media_dynamic_depth_out.ncl: Likewise.
    * tests/media_multi.ncl: Likewise.
    * tests/media_out.ncl: Likewise.
    * tests/mirror_example.ncl: Likewise.
    * tests/property.ncl: Likewise.
    * tests/property_right.ncl: Likewise.
    * tests/region.ncl: Likewise.
    * tests/region_out.ncl: Likewise.
    * tests/s3d_orig.ncl: Likewise.
    * util.lua: Likewise.

--- templates_xml.lua
+++ templates_xml.lua
@@ -1,3 +1,21 @@
+--[[ Copyright (C) 2015 Roberto Azevedo
+
+This file is part of ncls3d.
+
+ncls3d is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+ncls3d is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with ncls3d. If not, see <http://www.gnu.org/licenses/>.
+--]]
+
 local inspect = require "inspect"
 local util    = require "util"
 local slaxml  = require "slaxdom_ext"
